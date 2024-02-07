11191b380b45af31c6bdfa5731e032e7797bb4d7
sdnicck <158784055+sdnicck@users.noreply.github.com>
2024-02-07 08:53:26 -0800

    Update cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,5 +1,6 @@
 # author: satya dulam
 # lab 1 in CIS129 class
 # prints a simple story
+# print('hello world') deleted
 print('I parked on the 5th level of the parking garage at 8:30am. I took my backpack, purse, phone and lunch with me to class. I forgot my coffee today. After class I go back to the 5th floor of the parking garage. My car got stolen.')
 
