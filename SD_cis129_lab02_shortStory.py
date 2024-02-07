cecf309e228b12e72441572abdc2ad9170fcd59e
sdnicck <158784055+sdnicck@users.noreply.github.com>
2024-02-07 08:56:17 -0800

    Update and rename cis129_lab02_shortStory.py to SD_cis129_lab02_shortStory.py

--- SD_cis129_lab02_shortStory.py
+++ SD_cis129_lab02_shortStory.py
@@ -4,3 +4,4 @@
 # print('hello world') deleted
 print('I parked on the 5th level of the parking garage at 8:30am. I took my backpack, purse, phone and lunch with me to class. I forgot my coffee today. After class I go back to the 5th floor of the parking garage. My car got stolen.')
 
+
