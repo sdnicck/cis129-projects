498a7304322fea240460bcb9163f3b64fe34fc4d
sdnicck <158784055+sdnicck@users.noreply.github.com>
2024-02-23 12:23:35 -0800

    Create CIS129_sdulam_lab3.py

---
+++ CIS129_sdulam_lab3.py
@@ -0,0 +1,9 @@
+#A retail company assigns a $6,000 store bonus if monthly sales are more than $110,000; else if monthly sales are greater than or equal to $100,000 the store bonus is $5,000, else if monthly sales are greater than or equal to $90,000 the store bonus is $4,000, else if monthly sales are greater than or equal to $80,000, the store bonus is $3,000 otherwise a $0 amount or no store bonus is awarded.  They are using a percent of sales increase to determine if employees get individual bonuses.  If sales increased by an amount greater than or equal to 5% (0.05) then all employees get $75, else if sales increased by an amount greater than or equal to 4%, employees get $50, else if sales increased by an amount greater than or equal to 3% employees get $40 otherwise they get $0. 
+
+#storeAmount = the store bonus amount
+
+#empAmount = individual bonus amount 
+
+#salesIncrease = precent of increase
+
+#monthlySales = monthly sales amount
