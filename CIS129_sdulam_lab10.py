77f9026a9979f7d92f4da0adc3f5d2dcdc647ec0
sdnicck <158784055+sdnicck@users.noreply.github.com>
2024-05-01 10:04:03 -0700

    Add files via upload

---
+++ CIS129_sdulam_lab10.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[16]:
+
+
+#Satya Dulam
+#4/30/24
+#mod 10 lab
+
+def main():
+    
+    #collecting amount from user
+    dollarAmount = getFloat("Please input a dollar amount up to one cent:")
+
+    #converting input into a str with commas, a formated to the correct decimal point 
+    check = str(f'[{dollarAmount:*>10,.2f}]')
+    
+    #printing the input that is formated correctly
+    print('\n\n$...',check)
+
+# user input validation to only let the user enter a number
+def getFloat(message):
+    while True:
+        try:
+            dollarAmount = float(input(message))
+            return dollarAmount
+        except ValueError:
+             print("\nIncorrect data entered, please re-attempt")
+    
+main()
+
+
+# In[8]:
+
+
+#amount = "399.87" # this represents the dollar amount
+amount = input('Enter a dollar amount:')
+print(f'{amount:$>10}')
+
+
+# In[ ]:
+
+
+
+
